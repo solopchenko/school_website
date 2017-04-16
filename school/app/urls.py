795d894510eac13f01e7ed9d663f560465def0ac
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 15:40:53 +0300

    Добавлено представление для главной страницы

--- school/app/urls.py
+++ school/app/urls.py
@@ -15,7 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from core.views import HomeView
 
 urlpatterns = [
+
+    # Админка
+    #
     url(r'^admin/', admin.site.urls),
+
+    # Главная страница
+    #
+    url(r'^$', HomeView.as_view()),
+
 ]
--- school/core/views.py
+++ school/core/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+class HomeView(View):
+    def get(self, request):
+        template = 'base.html'
+        context = {}
+        return render(request, template, context)
