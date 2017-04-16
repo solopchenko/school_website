39d98c6c6d8245be36a173a86ffbf30c7e2b5e8f
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 15:22:24 +0300

    Добавлена модель сайта Site

--- school/core/models.py
+++ school/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Site(models.Model):
+    title = models.CharField(verbose_name='Полное наименование', max_length=200)
+    description = models.CharField(verbose_name='Полное наименование', max_length=500, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Настройки сайта'
+        verbose_name_plural = 'Настройки сайта'
