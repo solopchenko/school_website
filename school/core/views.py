2653b57a6e7c46f4bacaaf0a68c731dd6a9ff2a5
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 19:30:16 +0300

    Добавлен шаблон главной страницы

--- school/core/views.py
+++ school/core/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.views import View
 from .models import Site
+from news.models import Article
 
 # Create your views here.
 class HomeView(View):
     def get(self, request):
         page = Site.objects.all().first().homepage
+        articles = Article.objects.published()
 
-        context = {'page': page}
-        template = 'page.html'
+        context = { 'page': page, 'articles': articles }
+        template = 'home.html'
         return render(request, template, context)
