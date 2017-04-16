b9b087da89e93eb3928c43da89d38fe35ac6bdbf
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 16:20:06 +0300

    Настройка модели Site в админке

--- school/core/admin.py
+++ school/core/admin.py
@@ -2,4 +2,18 @@ from django.contrib import admin
 from .models import Site
 
 # Register your models here.
-admin.site.register(Site)
+class SiteAdmin(admin.ModelAdmin):
+
+    #Скрывает кнопку добавления нового элемента, если хотя бы один элемент уже есть
+    def has_add_permission(self, request):
+        if self.model.objects.count() > 0:
+            return False
+        return super(SiteAdmin, self).has_add_permission(request)
+
+    #Скрывает кнопку удаления последнего, если оставлся 1 элемент
+    def has_delete_permission(self, request, obj=None):
+        if self.model.objects.count() < 2:
+            return False
+        return super(SiteAdmin, self).has_delete_permission(request)
+
+admin.site.register(Site, SiteAdmin)
