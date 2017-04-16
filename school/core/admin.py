9619efe9fd7960bf18b692be13153b0ccf7f5573
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 13:59:21 +0300

    Отключение проверки количества объектов

--- school/core/admin.py
+++ school/core/admin.py
@@ -13,18 +13,18 @@ class OrganisationAdmin(admin.ModelAdmin):
 
     #Ограничение на добавление объектов.
     #Не может быть более 1 объекта.
-    def has_add_permission(self, request):
-        if self.model.objects.count() > 0:
-            return False
-        else:
-            super().has_add_permission(request)
+    #def has_add_permission(self, request):
+    #    if self.model.objects.count() > 0:
+    #        return False
+    #    else:
+    #        super(OrganisationAdmin, self).has_add_permission(request)
 
     #Ограничение на удаление объектов.
     #Нельзя удалить все объекты.
-    def has_delete_permission(self, request, obj=None):
-        if self.model.objects.count() == 1:
-            return False
-        else:
-            super().has_delete_permission(request)
+    #def has_delete_permission(self, request, obj=None):
+    #    if self.model.objects.count() == 1:
+    #        return False
+    #    else:
+    #        super(OrganisationAdmin, self).has_delete_permission(request)
 
 admin.site.register(Organisation, OrganisationAdmin)
