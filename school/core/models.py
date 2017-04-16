5f344a8cdc999fb16727da7706b10fe723315d89
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 15:54:50 +0300

    Изменение название полей модели Site

--- school/core/models.py
+++ school/core/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Site(models.Model):
-    title = models.CharField(verbose_name='Полное наименование', max_length=200)
-    description = models.CharField(verbose_name='Полное наименование', max_length=500, blank=True)
+    title = models.CharField(verbose_name='Заголовок сайта', max_length=200)
+    description = models.CharField(verbose_name='Описание сайта', max_length=500, blank=True)
 
     def __str__(self):
         return self.title
