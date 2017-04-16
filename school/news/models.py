b73b097fdd3fc2a313d55159d7dcf3cfb24cf6ec
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 18:31:24 +0300

    Изменение полей модели новости

--- school/news/models.py
+++ school/news/models.py
@@ -3,8 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Article(models.Model):
-    title = models.CharField(verbose_name='Заголовок', max_length=500)
-    announcement = models.TextField(verbose_name='Анонс', max_length=200, help_text='Отображается вверху новости. Форматирование текста <b>не поддерживается.</b>')
+    title = models.CharField(verbose_name='Заголовок', max_length=200)
+    announcement = models.TextField(verbose_name='Анонс', max_length=500, help_text='Отображается вверху новости. Форматирование текста <b>не поддерживается.</b>')
     content = models.TextField(verbose_name='Контент')
     created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
