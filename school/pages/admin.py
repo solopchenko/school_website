d9ba1348e5e131ac0413d7a2e9bbb32571297ca8
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 16:36:53 +0300

    Регистрация модели страницы (Page) в админке

--- school/pages/admin.py
+++ school/pages/admin.py
@@ -1,3 +1,31 @@
+
 from django.contrib import admin
+from .models import Page, Section
 
 # Register your models here.
+
+class SectionInline(admin.StackedInline):
+    model = Section
+    extra = 0
+
+
+class PageAdmin(admin.ModelAdmin):
+    fields = ('title', 'slug', 'parent', 'url', 'created_at', 'updated_at', )
+    readonly_fields = ('url', 'created_at', 'updated_at', )
+    list_display = ('title', 'url', 'created_at', 'updated_at',)
+    prepopulated_fields = {'slug': ('title', )}
+
+    inlines = (SectionInline, )
+
+    def save_model(self, request, obj, form, change):
+
+        obj.slug = obj.slug.lower()
+
+        if obj.parent:
+            obj.url = obj.parent.url + '/' + obj.slug
+        else:
+            obj.url = obj.slug
+
+        obj.save()
+
+admin.site.register(Page, PageAdmin)
