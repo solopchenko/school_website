1d100f9cdeec6fd71d2459872f391da56448b235
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 18:24:41 +0300

    Регистрация модели новости в админке

--- school/news/admin.py
+++ school/news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Article
 
 # Register your models here.
+class ArticleAdmin(admin.ModelAdmin):
+    fields = ('title', 'announcement', 'content', 'published', 'created_at', 'updated_at', )
+    readonly_fields = ('created_at', 'updated_at', )
+    list_display = ('title', 'published', 'created_at', 'updated_at', )
+
+admin.site.register(Article, ArticleAdmin)
