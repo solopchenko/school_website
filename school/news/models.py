f24f014855be10c6cb30048bedd3b26e7989bd09
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-16 18:24:19 +0300

    Добавлена модель новости Article

--- school/news/models.py
+++ school/news/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(verbose_name='Заголовок', max_length=500)
+    announcement = models.TextField(verbose_name='Анонс', max_length=200, help_text='Отображается вверху новости. Форматирование текста <b>не поддерживается.</b>')
+    content = models.TextField(verbose_name='Контент')
+    created_at = models.DateTimeField(verbose_name='Дата создания', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
+    published = models.BooleanField(verbose_name='Опубликовать', default=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
