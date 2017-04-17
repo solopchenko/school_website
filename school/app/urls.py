9f2553f4b48dd78a5bb5203a98c2e16be1ccc590
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-18 00:12:47 +0300

    Добавлены пути до загружаемых файлов

--- school/app/urls.py
+++ school/app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from core.views import HomeView
 from pages.views import PageView
@@ -38,3 +40,6 @@ urlpatterns = [
     #
     url(r'^(?P<page_url>[/\w-]*)/$', PageView.as_view()),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
