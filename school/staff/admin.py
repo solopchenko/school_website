2cfa4f0157f3e7275a8dc9e5cb6daf6dea4f67b2
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-18 23:32:21 +0300

    Регистрация UserProfile в админке

--- school/staff/admin.py
+++ school/staff/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import UserProfile
 
 # Register your models here.
+class UserProfileInLine(admin.StackedInline):
+    model = UserProfile
+
+class ExtendedUserAdmin(UserAdmin):
+    list_display = ('userprofile', 'username', 'is_active', )
+    inlines = (UserProfileInLine, )
+
+#Приложение auth
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+    pass
+
+#Приложение staff
+admin.site.register(UserProfile, UserProfileAdmin)
