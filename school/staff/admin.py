7c92cd511e88bb477f350e4be523ee12c024cfb9
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-21 00:16:32 +0300

    Настройка модели пользователя в админке

--- school/staff/admin.py
+++ school/staff/admin.py
@@ -1,23 +1,18 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import UserProfile
 
 # Register your models here.
 class UserProfileInLine(admin.StackedInline):
     model = UserProfile
+    can_delete = False
+    verbose_name_plural = 'Сотрудник'
 
-class ExtendedUserAdmin(UserAdmin):
-    list_display = ('userprofile', 'username', 'is_active', )
+class UserAdmin(BaseUserAdmin):
     inlines = (UserProfileInLine, )
+    list_display = ('username', 'userprofile', 'is_active', )
 
 #Приложение auth
 admin.site.unregister(User)
-admin.site.register(User, ExtendedUserAdmin)
-
-
-class UserProfileAdmin(admin.ModelAdmin):
-    pass
-
-#Приложение staff
-admin.site.register(UserProfile, UserProfileAdmin)
+admin.site.register(User, UserAdmin)
