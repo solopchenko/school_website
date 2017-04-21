7d7f6e126fbbbde46ab8776b4f203886e947ca7a
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-21 20:52:50 +0300

    Добавлена автоматическая запись автора статьи

--- school/news/admin.py
+++ school/news/admin.py
@@ -7,4 +7,9 @@ class ArticleAdmin(admin.ModelAdmin):
     readonly_fields = ('author', 'created_at', 'updated_at', )
     list_display = ('title', 'published', 'created_at', 'updated_at', )
 
+    def save_model(self, request, obj, form, change):
+        if not hasattr(obj, 'author'):
+            obj.author = request.user
+        super(ArticleAdmin, self).save_model(request, obj, form, change)
+
 admin.site.register(Article, ArticleAdmin)
