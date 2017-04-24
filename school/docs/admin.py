a2fc3c9b8e5a0529727d7fbf169768628dccb0fc
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-24 22:34:24 +0300

    Регистрация модели документа в админке

--- school/docs/admin.py
+++ school/docs/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Document
 
 # Register your models here.
+class DocumentAdmin(admin.ModelAdmin):
+    fields = ('title', 'paper', 'link', 'revised_at', 'created_at', 'updated_at', )
+    readonly_fields = ('created_at', 'updated_at', )
+    list_display = ('title', 'created_at', )
+
+admin.site.register(Document, DocumentAdmin)
