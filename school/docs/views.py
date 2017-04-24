abd836754d736b21c158b98a1ad59e324309296a
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-24 23:45:29 +0300

    Изменено представление списка документов

--- school/docs/views.py
+++ school/docs/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
-from .models import Document
+from .models import Category
 
 # Create your views here.
 def docs_list(request):
-    documents = Document.objects.all()
-    context = { 'documents': documents }
+    document_categories = Category.objects.public()
+    context = { 'document_categories': document_categories }
     template = 'docs/list.html'
     return render(request, template, context)
