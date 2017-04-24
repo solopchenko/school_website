51a702af11e28333008b0eddf0e48e150d345d69
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-24 23:44:42 +0300

    Добавлена модель категории документов

--- school/docs/admin.py
+++ school/docs/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Document
+from .models import Document, Category
 
 # Register your models here.
 class DocumentAdmin(admin.ModelAdmin):
-    fields = ('title', 'paper', 'link', 'revised_at', 'created_at', 'updated_at', )
+    fields = ('title', 'paper', 'link', 'revised_at', 'category', 'created_at', 'updated_at', )
     readonly_fields = ('created_at', 'updated_at', )
-    list_display = ('title', 'created_at', )
+    list_display = ('title', 'category', 'created_at', )
+    list_filter = ('category', 'category__is_public', )
 
 admin.site.register(Document, DocumentAdmin)
+admin.site.register(Category)
--- school/docs/models.py
+++ school/docs/models.py
@@ -2,8 +2,25 @@ from django.db import models
 from django.dispatch import receiver
 from django.db.models.signals import post_delete, pre_save, post_save
 from django.core.exceptions import ValidationError
+from .managers import CategoryManager
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(verbose_name='Наименование', max_length=200)
+    is_public = models.BooleanField(verbose_name='Отображать на странице документов', default=False)
+    description = models.TextField(verbose_name='Описание', blank=True)
+
+    def documents(self):
+        return self.document_set.all()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория документа'
+        verbose_name_plural = 'Категория документов'
+
+
 class Document(models.Model):
     title = models.CharField(verbose_name='Название документа', max_length=500)
     link = models.URLField(verbose_name='Ссылка на документ', blank=True)
@@ -11,6 +28,7 @@ class Document(models.Model):
     created_at = models.DateTimeField(verbose_name='Дата загрузки', auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name='Дата обновления', auto_now=True)
     revised_at = models.DateField(verbose_name='Дата последней редакции', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="Категория", blank=True, null=True)
 
     def __str__(self):
         return self.title
