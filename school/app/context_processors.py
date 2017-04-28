97dc33f2c3a9682357eae78284857e322bed2767
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-28 23:53:59 +0300

    Отображение меню в footer

--- school/app/context_processors.py
+++ school/app/context_processors.py
@@ -1,5 +1,9 @@
-from core.models import Site
+from core.models import Site, Menu
 
 def site(request):
     site = Site.objects.all().first()
-    return {'site': site}
+
+    top_menu = Menu.objects.top()
+    footer_menu = Menu.objects.footer()
+
+    return {'site': site, 'top_menu': top_menu, 'footer_menu': footer_menu}
