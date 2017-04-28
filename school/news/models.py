7276773487d2924ac1c4049cccceef6c4b9344e7
Svyatoslav Solopchenko <svyatoslav@Svyatoslavs-MacBook-Air.local>
2017-04-29 01:06:08 +0300

    Изменен get_absolute_url в модели новости

--- school/news/models.py
+++ school/news/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.core.urlresolvers import reverse
 from app.utils import get_now
 from .managers import ArticleManager
 
@@ -27,7 +28,7 @@ class Article(models.Model):
         super(Article, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
-        return "/news/{0}".format(self.pk)
+        return reverse('news_detail', args=[str(self.pk)])
 
     def __str__(self):
         return self.title
